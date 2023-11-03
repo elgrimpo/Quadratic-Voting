b20e12c80531ea2521b144c37cf665e6937005ce
Christoph <biedermann.chris@gmail.com>
2023-11-03 15:37:25 -0700

    add render page to cors

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use(passport.session());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://quadratic-voting.onrender.com/"],
     methods: "GET,POST,PUT,DELETE",
     credentials: true,
   })
